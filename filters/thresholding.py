1ab87ad1032c89bb86140ccd468261037b8f8ddd
Vinicius de Oliveira Jimenez <vinicius.jimenez@itaipuparquetec.org.br>
2025-05-12 14:18:37 -0300

    feat: thresholding

---
+++ filters/thresholding.py
@@ -0,0 +1,30 @@
+import sys
+import cv2
+import numpy as np
+
+
+def thresholding(
+    image: np.ndarray, threshold: int = 127, max_value: int = 255
+) -> np.ndarray:
+    if len(image.shape) == 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    copied_image = np.zeros_like(image)
+
+    copied_image[image > threshold] = max_value
+    copied_image[image <= threshold] = 0
+
+    return copied_image
+
+
+if __name__ == "__main__":
+    image = cv2.imread("mock/bobsin.jpg")
+
+    thresholded_image = thresholding(image, threshold=127, max_value=255)
+
+    cv2.imshow("Thresholded Image", thresholded_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+
+    sys.exit(0)
