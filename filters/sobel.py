52dcdeb50bc6bf1b7e6d9f5781f9aabdee9a903a
Vinicius de Oliveira Jimenez <vinicius.jimenez@itaipuparquetec.org.br>
2025-05-14 20:14:41 -0300

    feat: add cannyon edge filer

--- filters/sobel.py
+++ filters/sobel.py
@@ -6,15 +6,21 @@ from filters.grayscale import grayscale
 from utils.convolve import convolve
 
 
-def sobel(image: np.ndarray) -> np.ndarray:
-    if len(image.shape) == 3:
-        image = grayscale(image=image)
-
+def get_sobel_gradients(image: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32)
     kernel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], dtype=np.float32)
 
     gradient_x = convolve(image=image, kernel=kernel_x)
     gradient_y = convolve(image=image, kernel=kernel_y)
+
+    return gradient_x, gradient_y
+
+
+def sobel(image: np.ndarray) -> np.ndarray:
+    if len(image.shape) == 3:
+        image = grayscale(image=image)
+
+    gradient_x, gradient_y = get_sobel_gradients(image=image)
 
     result = np.sqrt(gradient_x**2 + gradient_y**2)
     result = np.clip(result, 0, 255).astype(np.uint8)
